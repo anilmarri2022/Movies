f706c77f5016e882eee675590744ee9e552cb30d
anilgithu <anilstudent1999@gmail.com>
2023-05-07 16:55:16 +0530

    third commit

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import "./Main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
 import Netflixlogo from "../../src/images/Netflixlogo.jpg";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const Main = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
@@ -14,14 +20,14 @@ const Main = () => {
       .then((value) => setData(value.Search));
   };
 
-  
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark   header">
+      {/* <nav className="navbar navbar-expand-lg navbar-dark   header">
         <Link to="/">
-        {" "}
-          <img className="header__icon" src={Netflixlogo} alt="img"/>
+          {" "}
+          <img className="header__icon" src={Netflixlogo} alt="img" />
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -52,6 +58,7 @@ const Main = () => {
             </li>
           </ul>
         </div>
+
         <form class="d-flex" role="search" onSubmit={searchHandler}>
           <input
             class="form-control me-2"
@@ -65,7 +72,54 @@ const Main = () => {
             Search
           </button>
         </form>
-      </nav>
+      </nav> */}
+      
+            {/* <form class="d-flex" role="search" onSubmit={searchHandler}>
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form> */}
+  <Navbar className="navbar expand-lg navbar-dark   header" expand="lg">
+      <Container>
+      <Navbar.Brand href="/">
+      <Image className="header__icon" src={Netflixlogo} alt="img" />
+      </Navbar.Brand>
+      
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/movies/popular"><span>Popular</span></Nav.Link>
+            <Nav.Link href="/movies/top_rated"><span>Top Rated</span></Nav.Link>
+            <Nav.Link href="/movies/upcoming"><span>Upcoming</span></Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+        <form class="d-flex" role="search" onSubmit={searchHandler}>
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+    </Navbar>
+    
+      
+
       <div className="row">
         {data.map((movie) => (
           <div className="col-md-3">
